055bc3a85f525d9d6c789fa5f092ce2cd254fc80
Justin Wheeler <justin.wheeler@gfs.com>
2024-07-01 16:29:43 -0400

    Updated Tags

--- bin/serverless-analytics.ts
+++ bin/serverless-analytics.ts
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ServerlessAnalyticsStack } from '../lib/serverless-analytics-stack';
-import {Tags} from "aws-cdk-lib";
+import {ServerlessAnalyticsStack} from '../lib/serverless-analytics-stack';
+import {Tags} from 'aws-cdk-lib';
+
+const environmentName = 'productionA';
+const serviceName = 'ServerlessAnalyticsStack';
 
 const app = new cdk.App();
-
 const swaStack = new ServerlessAnalyticsStack(app, 'serverless-analytics', {
   env: {
     account: '590183969109',
     region: 'us-east-2',
   },
+  tags: {
+    'Environment': environmentName,
+    'Service': serviceName
+  }
 });
 
-Tags.of(swaStack).add('App', 'serverless-analytics');
+Tags.of(swaStack).add('Environment', environmentName);
+Tags.of(swaStack).add('Name', `${environmentName}/${serviceName}`);
+Tags.of(swaStack).add('Service', serviceName);
--- lib/serverless-analytics-stack.ts
+++ lib/serverless-analytics-stack.ts
@@ -1,21 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct, IConstruct} from 'constructs';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import {AllAlarmTypes, Swa} from 'serverless-website-analytics';
+import {Tags} from 'aws-cdk-lib';
 
 export class ServerlessAnalyticsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const alarmTopic = new sns.Topic(this, "alarm-topic");
+    const snsTopic = new sns.Topic(this, 'alarm-topic');
+    this.addTags({
+      name: 'SnsTopic',
+      resource: snsTopic,
+      tags: props?.tags,
+    })
 
-    new sns.Subscription(this, "alarm-topic-subscription", {
-      topic: alarmTopic,
+    const snsSubscription = new sns.Subscription(this, 'alarm-topic-subscription', {
+      topic: snsTopic,
       protocol: sns.SubscriptionProtocol.EMAIL,
       endpoint: 'justin.wheeler@wheeleruniverse.com',
     });
+    this.addTags({
+      name: 'SnsSubscription',
+      resource: snsSubscription,
+      tags: props?.tags,
+    })
 
-    new Swa(this, 'serverless-analytics', {
+    const swa = new Swa(this, 'serverless-analytics', {
       allowedOrigins: ['https://wheelerrecommends.com'],
       awsEnv: {
         account: this.account,
@@ -25,7 +36,7 @@ export class ServerlessAnalyticsStack extends cdk.Stack {
       observability: {
         dashboard: true,
         alarms: {
-          alarmTopic,
+          alarmTopic: snsTopic,
           alarmTypes: AllAlarmTypes
         },
       },
@@ -34,6 +45,25 @@ export class ServerlessAnalyticsStack extends cdk.Stack {
         'wheelerrecommends.com',
       ],
     });
+    this.addTags({
+      name: 'Swa',
+      resource: swa,
+      tags: props?.tags
+    });
+  }
 
+  private addTags({name, resource, tags}: {
+    name: string,
+    resource: IConstruct,
+    tags?: { [p: string]: string } | undefined
+  }): void {
+    const environmentName = tags?.['Environment'];
+    if (environmentName) {
+      Tags.of(resource).add('Environment', environmentName);
+    }
+
+    const parent = tags?.['Service'];
+    Tags.of(resource).add('Name', parent ? `${parent}/${name}` : name);
+    Tags.of(resource).add('Service', name);
   }
 }
